99b9748443161a2088cd83b318c9951b39db243d
Lihyrs <llg4192@126.com>
2019-04-02 22:34:14 +0800

    refact controlUnit

--- controlUnit.js
+++ controlUnit.js
@@ -1,4 +1,13 @@
 import Raw from './raw'
+import { uint8, OPCODES,MARKS,BIT_WIDTHS} from './common/types'
+import { CPU_EVENTS } from './common/events'
+
+const createEvent = function(type,payload){
+    return {
+        type: type,
+        payload: typeof payload === 'object' ? {...payload} : {value: payload}
+    }
+};
 
 // simple control unit
 class controlUnit {
@@ -15,50 +24,28 @@ class controlUnit {
          * instruction address register,
          * stores the memory address of the current instruction
          * 指令地址寄存器
-        */
+         */
         this.instAddrRegister = uint8();
 
         this.ALUFlags = flags;
+
+        this.tmpRegister = uint8();
     }
 
-    decodePhase(){
-        let mask = ~uint8();
-        // let instVal = this.controlUnit.getInstVal();
-        let instVal = this.instRegister;
-        let operaCode;
-        mask = mask<<4;
-        operaCode = instVal & mask;
-        operaCode = operaCode>>>4;
-        return operaCode;
-    }
-
-    executePhase(){
-        const {ADD,SUB,LOAD_A,LOAD_B} = operaCodes;
-        switch(operaCode){
-            case ADD:
-            case SUB:
-           this.computeByALU(operaCode);
-            break;
-            case LOAD_A:
-            break;
-            case LOAD_B:
-            break;
-        }
-        //do something
-        this.instAddrRegister++;
-        //end
-    }
-
-    doGetInstVal() {
-        this.instRegister = Raw.get(this.instAddrRegister);
-    }
-
-    getInstAddress() {
+    getInstAddressRegisterVal() {
         return this.instAddrRegister;
     }
 
-    getInstVal() {
+    setInstAddressRegister(addr) {
+        this.instAddrRegister = addr;
+    }
+
+    getInstRegisterVal() {
         return this.instRegister;
+    }
+
+    setInstRegister(val) {
+        this.instRegister = val;
     }
 
     setALUFlags(obj = {}) {
@@ -67,7 +54,80 @@ class controlUnit {
             ...obj
         }
     }
-    setInstAddress(val){
-        this.instAddrRegister = val;
+
+    getTmpRegister(){
+        return this.tmpRegister;
+    }
+
+    setTmpRegisterVal(val){
+        this.tmpRegister = val;
+    }
+
+    getValByRaw(addr) {
+        return Raw.get(addr);
+    }
+
+    getOpcodeByInstRegisterVal() {
+        const {BIT_WIDTH,OPCODE_WIDTH} = BIT_WIDTHS;
+
+        let instVal = this.instRegister;
+        let opcode;
+        opcode = instVal & MARKS.OPCODE_MARK;
+
+        opcode = opcode >>> (BIT_WIDTH - OPCODE_WIDTH);
+
+        return opcode;
+    }
+
+    getPayloadByInstRegisterVal(){
+        const {BIT_WIDTH,OPCODE_WIDTH} = BIT_WIDTHS;
+
+        let instVal = this.instRegister;
+        let payload;
+        payload = instVal & MARKS.PAYLOAD_MARK;
+
+        return payload;
+    }
+
+    executeByOpcode(opcode) {
+        const {
+            ADD,
+            SUB,
+            LOAD_A,
+            LOAD_B
+        } = OPCODES;
+
+        const {
+            COMPUTE_BY_ALU,
+            SET_REGISTER
+        } = CPU_EVENTS;
+
+        const {PAYLOAD_LHS_MARK,PAYLOAD_RHS_MARK} = MARKS;
+
+        const input = this.instRegister;
+        let ret = [];
+        switch (opcode) {
+            case ADD:
+            case SUB:
+                const payload = this.getPayloadByInstRegisterVal();
+                const rhs= payload & PAYLOAD_RHS_MARK;
+                const lhs= payload & PAYLOAD_LHS_MARK;
+
+                ret = createEvent(COMPUTE_BY_ALU, {
+                    opcode,
+                    rhs,
+                    lhs,
+                    des: rhs,
+                })
+                break;
+            case LOAD_A:
+            ret = createEvent(SET_REGISTER,{address: 0,value:0})
+                break;
+            case LOAD_B:
+            ret = createEvent(SET_REGISTER,{address: 1,value:0})
+                break;
+        }
+
+        return ret;
     }
 }
